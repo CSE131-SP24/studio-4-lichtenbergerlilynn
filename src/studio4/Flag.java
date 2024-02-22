36f6dd64639584e6dd98219600beaf8cff70dda3
HP <HP@DESKTOP-J58UB0Q.dhcp.wustl.edu>
2024-02-22 11:15:57 -0600

    Studio4 Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+
+		StdDraw.setPenColor(0,100,255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.1);
+		StdDraw.setPenColor(0,255,0);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+		StdDraw.setPenColor(150,50,50);
+		StdDraw.arc(0.6, 0.5, 0.1, 270, 360);
+		StdDraw.arc(0.4, 0.5, 0.1, 180, 270);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,35 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape=in.next();
+		int redcomp=in.nextInt();
+		int greencomp=in.nextInt();
+		int bluecomp=in.nextInt();
+		boolean isFilled=in.nextBoolean();
+		double x=in.nextDouble();
+		double y=in.nextDouble();
 		
+		if(shape.equals("rectangle")) {
+		double halfWidth=in.nextDouble();
+		double halfHeight=in.nextDouble();
+			if(isFilled==false) {
+				StdDraw.setPenColor(redcomp,greencomp,bluecomp);
+				StdDraw.rectangle(x,y,halfWidth,halfHeight);
+			}
+			else if(isFilled==true) {
+				StdDraw.setPenColor(redcomp,greencomp,bluecomp);
+				StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+			}}
+		else if(shape.equals("ellipse")){
+		double semiMajorAxis=in.nextDouble();
+		double semiMinorAxis=in.nextDouble();
+			if(isFilled==false) {
+				StdDraw.setPenColor(redcomp,greencomp,bluecomp);
+				StdDraw.ellipse(x,y,semiMajorAxis,semiMinorAxis);
+			}
+			else if(isFilled==true) {
+				StdDraw.setPenColor(redcomp,greencomp,bluecomp);
+				StdDraw.filledEllipse(x,y,semiMajorAxis,semiMinorAxis);
+			}}
 	}
 }
